65751ab978146a81145994ccf94e050540cccbe8
smnlin <lwxlin@gmail.com>
2013-01-23 23:46:32 +0800

    Initial Commit

---
+++ Employee/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Employee
+//
+//  Created by Simon Lin on 1/23/13.
+//  Copyright (c) 2013 Simon Lin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
